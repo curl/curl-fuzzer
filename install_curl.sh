a0cee294cf4be1d65b4037fa1033d6629076a259
Max Dymond <cmeister2@gmail.com>
2017-09-22 09:55:23 +0100

    travis: build and install curl
    Build and install curl to a temporary folder.

--- install_curl.sh
+++ install_curl.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+# If any commands fail, fail the script immediately.
 set -ex
 
-git clone http://github.com/curl/curl /tmp/curl
-ls /tmp/curl
+# Make an install target for curl.
+mkdir /tmp/curl_install
 
+git clone http://github.com/curl/curl /tmp/curl
+
+pushd /tmp/curl
+
+# Build the library.
+export CC=clang
+export CXX=clang++
+export CFLAGS="-fsanitize=address"
+export CXXFLAGS="-fsanitize=address -stdlib=libstdc++"
+
+./buildconf
+./configure --prefix=/tmp/curl_install --disable-shared --enable-debug --enable-maintainer-mode
+make
+make install
+
+popd
