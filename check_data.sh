2f270aeb5cb4eff49d944785874e704f5ecdbc66
Max Dymond <cmeister2@gmail.com>
2017-09-30 14:48:00 +0100

    oss-fuzz: Add a simple gopher:// URL
    Teach oss-fuzz about the gopher:// URL.

--- check_data.sh
+++ check_data.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 DEBUG=0
+
+# Allows us to add an extra testing corpus locally.
+if [[ -d ./extra_corpus ]]
+then
+    EXTRA_CORPUS=./extra_corpus/*
+else
+    EXTRA_CORPUS=
+fi
 
 if [[ ${DEBUG} == 1 ]]
 then
   # Run each test individually so we can see where it crashes
-  for ii in curl_fuzz_data/*
+  for ii in curl_fuzz_data/* ${EXTRA_CORPUS}
   do
     ./curl_fuzzer $ii
   done
 else
   # Run the fuzzer over all tests at once, which is faster.
-  ./curl_fuzzer curl_fuzz_data/*
+  ./curl_fuzzer curl_fuzz_data/* ${EXTRA_CORPUS}
 fi
