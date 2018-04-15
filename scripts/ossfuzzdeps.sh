21c2ffbbbcafe74861ea9da6097084c90e617c09
Max Dymond <cmeister2@gmail.com>
2018-04-15 18:55:20 +0100

    oss-fuzz: Use curl-fuzzer codebase to determine installed oss-fuzz dependencies

---
+++ scripts/ossfuzzdeps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ex
+
+# Download dependencies for oss-fuzz
+apt-get update
+apt-get install -y make \
+                   autoconf \
+                   automake \
+                   libtool \
+                   libssl-dev \
+                   zlib1g-dev
