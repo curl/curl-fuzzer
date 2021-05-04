4364c65debb970a345bb316e1da11837abc7472b
Andrea Fioraldi <andreafioraldi@gmail.com>
2021-05-04 17:22:32 +0200

    solve download_zlib failure

--- scripts/download_zlib.sh
+++ scripts/download_zlib.sh
@@ -3,7 +3,10 @@
 # If any commands fail, fail the script immediately.
 set -ex
 
-wget https://www.zlib.net/zlib-1.2.11.tar.gz -O /tmp/zlib-1.2.11.tar.gz
+# Seems that https://www.zlib.net/zlib-1.2.11.tar.gz is now behind a proxy and
+# wget downloads an HTML page instead of the archive.
+# As a workaround, download this same archive (same hash) from the LIBPNG mirror
+wget https://kumisystems.dl.sourceforge.net/project/libpng/zlib/1.2.11/zlib-1.2.11.tar.gz -O /tmp/zlib-1.2.11.tar.gz
 tar -xvf /tmp/zlib-1.2.11.tar.gz --directory /tmp
 
 # Copy the directory into the correct place
