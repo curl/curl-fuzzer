02cdc25ef95622f5c889adeecfd146206d62cfa2
Daniel Stenberg <daniel@haxx.se>
2022-03-29 12:54:07 +0200

    download_zlib.sh: use 1.2.12 now
    The 1.2.11 tarball has been removed!

--- scripts/download_zlib.sh
+++ scripts/download_zlib.sh
@@ -3,8 +3,8 @@
 # If any commands fail, fail the script immediately.
 set -ex
 
-wget https://www.zlib.net/zlib-1.2.11.tar.gz -O /tmp/zlib-1.2.11.tar.gz
-tar -xvf /tmp/zlib-1.2.11.tar.gz --directory /tmp
+wget https://www.zlib.net/zlib-1.2.12.tar.gz -O /tmp/zlib-1.2.12.tar.gz
+tar -xvf /tmp/zlib-1.2.12.tar.gz --directory /tmp
 
 # Copy the directory into the correct place
-mv -v /tmp/zlib-1.2.11 $1
+mv -v /tmp/zlib-1.2.12 $1
