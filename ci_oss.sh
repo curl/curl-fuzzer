32fc19a4fa6398ff9a23b744c7cf21547b375c6a
Max Dymond <cmeister2@gmail.com>
2022-09-13 11:00:00 +0100

    Fix ws:// url to be localhost, and fix Dockerfile modification
    The ci_oss.sh script wasn't usefully editing the Dockerfile - now it
    uses the checked-out code in the Dockerfile build.

--- ci_oss.sh
+++ ci_oss.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
+# Store the current directory.
+CURL_FUZZER_DIR=$(pwd)
+
 PROJECT_NAME=curl
 
 # Clone the oss-fuzz repository
@@ -10,11 +13,13 @@ git clone https://github.com/google/oss-fuzz.git /tmp/ossfuzz
 # Work out which branch to clone from, inside Docker
 BRANCH=${GITHUB_REF}
 
-# Modify the oss-fuzz Dockerfile so that we're checking out the current reference on CI.
-sed -i "s@RUN git clone --depth 1 https://github.com/curl/curl-fuzzer.git /src/curl_fuzzer@RUN git config --global remote.origin.fetch '+refs/pull/*:refs/remotes/origin/pull/*' \&\& git clone https://github.com/curl/curl-fuzzer.git /src/curl_fuzzer \&\& cd /src/curl_fuzzer \&\& git checkout -b ${BRANCH}@" /tmp/ossfuzz/projects/${PROJECT_NAME}/Dockerfile
+# Modify the oss-fuzz Dockerfile so that we're using the checked-out code from CI
+sed -i "s@RUN git clone --depth 1 https://github.com/curl/curl-fuzzer.git /src/curl_fuzzer@COPY ./curl_fuzzer /src/curl_fuzzer@" /tmp/ossfuzz/projects/${PROJECT_NAME}/Dockerfile
 
-# Try and build the fuzzers
+# Try and build the fuzzers. Need to copy the fuzzer directory to the
+# build context first.
 pushd /tmp/ossfuzz
+cp -r ${CURL_FUZZER_DIR} projects/curl/curl_fuzzer
 python3 infra/helper.py build_image --pull ${PROJECT_NAME}
 python3 infra/helper.py build_fuzzers ${PROJECT_NAME}
 python3 infra/helper.py check_build ${PROJECT_NAME} --engine libfuzzer --sanitizer address --architecture x86_64
