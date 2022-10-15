6673b3972f18c10c369bc14f6fb4943710e1bdaf
Viktor Szakats <commit@vsz.me>
2022-10-15 12:43:53 +0000

    link to the always latest source URL

--- scripts/download_zlib.sh
+++ scripts/download_zlib.sh
@@ -3,10 +3,7 @@
 # If any commands fail, fail the script immediately.
 set -ex
 
-latest="$(wget https://www.zlib.net/ -O - |
-  grep -o -E 'zlib-[0-9.]+\.tar.gz' | sort -u -r | head -1)"
-
-wget "https://www.zlib.net/${latest}" -O /tmp/src.tar.gz
+wget https://www.zlib.net/zlib.tar.gz -O /tmp/src.tar.gz
 tar -xvf /tmp/src.tar.gz --directory /tmp
 
 # Move the directory into the correct place
