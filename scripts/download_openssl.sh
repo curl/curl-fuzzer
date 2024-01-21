684e7190e23197eca1c09b60a7a9540da4e1768b
Max Dymond <cmeister2@gmail.com>
2024-01-21 21:54:04 +0000

    Just use the bare versions in the versions file.

--- scripts/download_openssl.sh
+++ scripts/download_openssl.sh
@@ -8,4 +8,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $SCRIPT_DIR/VERSIONS
 
 # Clone the repository to the specified directory.
-git clone --depth 1 --branch ${OPENSSL_VERSION} https://github.com/openssl/openssl $1
+git clone --depth 1 --branch openssl-${OPENSSL_VERSION} https://github.com/openssl/openssl $1
