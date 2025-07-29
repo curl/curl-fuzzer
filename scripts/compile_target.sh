57486962b45721adb9cd0078e1a04ec69b35034c
Viktor Szakats <commit@vsz.me>
2025-07-29 03:30:22 +0200

    try install ninja

--- scripts/compile_target.sh
+++ scripts/compile_target.sh
@@ -59,6 +59,8 @@ export MAKEFLAGS+='-j5'
 BUILD_DIR=${BUILD_ROOT}/build
 mkdir -p ${BUILD_DIR}
 
+apt-get --option Dpkg::Use-Pty=0 --yes install --no-install-suggests --no-install-recommends ninja-build
+
 # Compile the dependencies.
 pushd ${BUILD_DIR}
 cmake ${CMAKE_GDB_FLAG} .. -G Ninja
