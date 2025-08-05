47e88f3c7afd4e1a7da09745e2d0991b9d99eb58
Viktor Szakats <commit@vsz.me>
2025-08-05 23:11:09 +0200

    not much to gain with ninja after all, everything is gnu make, except the orchestrator which is incompatible with ninja due to using Externalproject

--- scripts/compile_target.sh
+++ scripts/compile_target.sh
@@ -59,11 +59,8 @@ export MAKEFLAGS+='-j5'
 BUILD_DIR=${BUILD_ROOT}/build
 mkdir -p ${BUILD_DIR}
 
-options=''
-#command -v ninja >/dev/null 2>&1 && options+=' -G Ninja'
-
 # Compile the dependencies.
 pushd ${BUILD_DIR}
-cmake ${CMAKE_GDB_FLAG} .. ${options}
+cmake ${CMAKE_GDB_FLAG} ..
 cmake --build . --target ${TARGET} ${CMAKE_VERBOSE_FLAG}
 popd
